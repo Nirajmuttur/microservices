7ed53806e647d71b29780352c387af30241e7421
nirajmuttur <nirajmuttur@gmail.com>
2022-11-21 19:00:14 +0530

    added config server

--- factory-service/src/main/java/com/example/factoryservice/controller/FactoryController.java
+++ factory-service/src/main/java/com/example/factoryservice/controller/FactoryController.java
@@ -4,7 +4,6 @@ import com.example.factoryservice.model.Factory;
 import com.example.factoryservice.service.FactoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
